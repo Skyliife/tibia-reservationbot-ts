7a7402a86cda469067e596699d59fb04798bac5f
skylife <no@mail.com>
2023-11-30 22:19:12 +0100

    added booking controller

--- src/bookingservice/bookingservice.ts
+++ src/bookingservice/bookingservice.ts
@@ -1,16 +1,18 @@
-import { InteractionReplyOptions } from "discord.js";
+import { ChatInputCommandInteraction } from "discord.js";
 
 class BookingService {
-    private bookings: Booking[] = [];
+  private bookings: Booking[] = [];
+  private options: { [key: string]: string | number | boolean } = {};
 
-
-/**
- *
- */
-constructor(options :InteractionReplyOptions) {
-    
+  /**
+   *
+   */
+  constructor(interaction: ChatInputCommandInteraction) {
+    for (let i = 0; i < interaction.options.data.length; i++) {
+      const element = interaction.options.data[i];
+      if (element.name && element.value) this.options[element.name] = element.value;
+    }
+  }
 }
 
-
-
-}  
+export default BookingService;
--- src/commands/book.ts
+++ src/commands/book.ts
@@ -1,12 +1,40 @@
 import { SlashCommandBuilder } from "discord.js";
 import { SlashCommand } from "../types";
+import BookingService from "../bookingservice/bookingservice";
+import logger from "../logging/logger";
 
 const command: SlashCommand = {
-  command: new SlashCommandBuilder().setName("book").setDescription("book a huntingground"),
-  execute: (interaction) => {
-    interaction.reply({
-      content: "Pong",
-    });
+  command: new SlashCommandBuilder().setName("book").setDescription("book a hunting ground"),
+  autocomplete: async (interaction) => {
+    try {
+      const focusedValue = interaction.options.getFocused();
+      const choices = [{ name: "test", value: "test" }];
+      let filtered: { name: string; value: string }[] = [];
+      for (let i = 0; i < choices.length; i++) {
+        const choice = choices[i];
+        if (choice.name.includes(focusedValue)) filtered.push(choice);
+      }
+    } catch (error) {
+      logger.error(`Error: ${error}`);
+    }
+  },
+  execute: async (interaction) => {
+    logger.info("Start executing /book command!");
+    try {
+      await interaction.deferReply({ ephemeral: true });
+      if (!interaction.options) {
+        logger.error("Interaction Options are null/undefined");
+        return interaction.editReply({ content: "Something went wrong..." });
+      }
+
+      const bookingService = new BookingService(interaction);
+      interaction.reply({
+        content: "Pong",
+      });
+    } catch (error) {
+      logger.error(error);
+      interaction.editReply({ content: "Something went wrong..." });
+    }
   },
 };
 
